76d62703cf8736257bd027c6c3565cd3b740bb94
Miranda Kachlavashvili <136162510+Miranda-K12@users.noreply.github.com>
2025-05-22 00:12:44 +0400

    change header.js

--- js/header.js
+++ js/header.js
@@ -2,29 +2,31 @@ export const scrollHeader = () => {
   const header = document.querySelector(".header");
   const headerText = header.querySelectorAll(".nav__link");
   const burgerMenu = document.querySelector(".burger-menu");
-  const navMenu = document.querySelector(".nav");
+
   const changeBackground = () => {
+    const smallScreen = window.innerWidth < 900;
+
     if (window.scrollY > 80) {
       header.style.backgroundColor = "#d1ecfe";
       headerText.forEach((element) => {
-        element.style.color = "#0d6efd";
-
-        if (burgerMenu) {
-          burgerMenu.style.color = "#0d6efd";
-        }
+        element.style.color = smallScreen ? "#ffffff" : "#0d6efd";
       });
+
+      if (burgerMenu) {
+        burgerMenu.style.color = "#0d6efd";
+      }
     } else {
       header.style.backgroundColor = "#0d6efd";
       headerText.forEach((element) => {
         element.style.color = "#ffffff";
-        if (burgerMenu) {
-          burgerMenu.style.color = "#ffffff";
-        }
       });
+
+      if (burgerMenu) {
+        burgerMenu.style.color = "#ffffff";
+      }
     }
   };
 
   window.addEventListener("scroll", changeBackground);
 };
-
 scrollHeader();
